e168f02d2f8060c66799a1a4596e5f96a8ccd562
neqbal <nooraineqbal@gmail.com>
2024-08-15 00:39:53 +0530

    .

--- FullStackJS/ToDoList/src/index.js
+++ FullStackJS/ToDoList/src/index.js
@@ -2,16 +2,24 @@ import './style.css';
 import { createNote } from './notes';
 
 const main = () => {
+    setLocalStorage();
     const note = document.querySelector("#note");
     const folder = document.querySelector("#folder");
+    const tdl = document.querySelector(".ToDo-List-container");
 
     note.addEventListener('click', () => {
-        createNote();
+        createNote(tdl);
     });
 
-    folder.addEventListener('click', ()=> {
+    folder.addEventListener('click', () => {
         prompt("add folder");
     });
+};
+
+function setLocalStorage() {
+    localStorage.clear();
+    var ob = {};
+    localStorage.setItem("", JSON.stringify(ob));
 }
 
 main();
--- FullStackJS/ToDoList/src/notes.js
+++ FullStackJS/ToDoList/src/notes.js
@@ -1,6 +1,34 @@
-const createNote = () => {
-    const currProject = "";
-    const currNote = "";
+const createNote = (tdl) => {
+    const NameContainer = document.createElement("div");
+    NameContainer.setAttribute("class", "name-container");
+
+    const name = document.createElement("input");
+    name.setAttribute("class", "name-area");
+    name.setAttribute("type", "text");
+    name.setAttribute("maxlength", "12");
+
+    const more_container = document.createElement("div");
+    more_container.setAttribute("class", "more-container");
+    
+    NameContainer.appendChild(name);
+    NameContainer.appendChild(more_container);
+    tdl.appendChild(NameContainer);
+
+    name.addEventListener("keypress", function(e) {
+        if(e.key === 'Enter') {
+            addList(name);
+        }
+    });
+};
+
+const addList = (name) => {
+    localStorage.setItem(name.value, "");
+    const NameContainer = name.parentNode;
+    const div = document.createElement("div");
+    div.innerHTML=name.value;
+
+    NameContainer.insertBefore(div, name);
+    NameContainer.removeChild(name);
 };
 
 export {createNote};
