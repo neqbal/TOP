81526798b5d2cad41854759179d5b44d59d65c71
neqbal <nooraineqbal@gmail.com>
2024-09-01 15:23:39 +0530

    .

--- FullStackJS/weatherAPI/index.js
+++ FullStackJS/weatherAPI/index.js
@@ -9,8 +9,39 @@ input.addEventListener("keyup", function(e) {
 });
 
 async function getData(location) {
-    const weatherinfo = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/udupi/?key=P32GYF38FG38FG5ME7KCXLFYK");
-    const data = await weatherinfo.json();
-    container.innerHTML=JSON.stringify(data);
-    console.log(data);
+    const weatherinfo = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/?key=P32GYF38FG38FG5ME7KCXLFYK");
+    if(weatherinfo.status === 200) {
+        const data = await weatherinfo.json();
+        arrangeData(data);    
+        console.log(data);
+    } else {
+        console.log("no data available");
+    }
+}
+
+function arrangeData(data) {
+    document.querySelector("#address").innerHTML=data.resolvedAddress;
+    document.querySelector('#currWeather').innerHTML=data.currentConditions.temp + "°F";
+    document.querySelector('#conditions').innerHTML=data.currentConditions.conditions;
+    const forecast = document.querySelector('.five_day_forecast');
+    forecast.innerHTML="";
+    for(var i=1; i<=5; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'day' + i);
+        
+        const date = document.createElement('div');
+        date.setAttribute('id', 'date');
+        date.innerHTML=data.days[i-1].datetime;
+        const temp = document.createElement('div');
+        temp.setAttribute('id', 'temp');
+        temp.innerHTML=data.days[i-1].temp;
+        const cond = document.createElement('div');
+        cond.setAttribute('id', 'cond');
+        cond.innerHTML=data.days[i-1].conditions;
+
+        div.appendChild(date);
+        div.appendChild(temp);
+        div.appendChild(cond);
+        forecast.appendChild(div);
+    }
 }
