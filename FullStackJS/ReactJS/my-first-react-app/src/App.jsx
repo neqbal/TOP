f2ca4a9e1c74dafbbd2409e586ff2c75fef8981b
neqbal <nooraineqbal@gmail.com>
2024-10-03 18:56:41 +0530

    .

--- FullStackJS/ReactJS/my-first-react-app/src/App.jsx
+++ FullStackJS/ReactJS/my-first-react-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function ListItem(props) {
+                
+/*function ListItem(props) {
 	return <li> {props.animal}</li>
 }
 
@@ -15,15 +15,65 @@ function List(props) {
 			})}
 		</ul>
 	)	
-}
+}*/
+/*import React, { useState } from "react";
+import "./App.css";
+
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+
 function App() {
-  const [count, setCount] = useState(0)
-  const animals = ["lion", "cheetahs", "lizard", "snake"];
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default App;*/
+const colors = ["red", "blue", "yellow"];
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  }
   return (
     <>
-      <div>
-        <h1> animals: </h1>
-	  <List animals = {animals} />
+      <div
+        className = "App"
+        style = {{
+          backgroundColor,
+        }}
+      >
+        {colors.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={onButtonClick(color)}
+            className={backgroundColor===color? "selected" : ""}
+          >
+          {color}
+          </button>
+        ))}
       </div>
     </>
   )
