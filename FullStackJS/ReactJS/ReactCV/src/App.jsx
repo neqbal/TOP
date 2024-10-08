4ff1c3125c9e61adf0c64591bb6d578b9140ff98
neqbal <nooraineqbal@gmail.com>
2024-10-08 23:34:52 +0530

    .

--- FullStackJS/ReactJS/ReactCV/src/App.jsx
+++ FullStackJS/ReactJS/ReactCV/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import gitHub from './assets/github.svg'
+import twitter from './assets/twitter.svg'
+import linkedin from './assets/linkedin.svg'
+import gmail from './assets/gmail.svg'
 import './App.css'
 
-function Info({text, color, fontSize, method}) {
+function SVGIcon({icon}) {
+  return (
+    <img  width='30px' height='30px' src={icon}></img>
+  )
+}
+
+function Info({className,text, color, fontSize, method}) {
   const infoStyle = {
     color: color,
     fontSize: fontSize + "rem"
   };
 
   return (
-    <div style={infoStyle} onDoubleClick={method}> {text}</div>
+    <div className={className} style={infoStyle} onDoubleClick={method}> {text}</div>
   )
 }
 
@@ -25,7 +35,7 @@ function Profession() {
 
   function handleKeyUp(e) {
     if(e.key === 'Enter') {
-      setProf(<Info text={e.target.value} color="" fontSize="5" method={doubleClickHandler} />)
+      setProf(<Info text={e.target.value} color="gray" fontSize="5" method={doubleClickHandler} />)
     }
   }
 
@@ -104,38 +114,72 @@ function ProjectDescription({defaultDesc}) {
 
   function handleKeyUp(e) {
     if(e.key === 'Enter') {
-      setDesc(<Info text={e.target.value} color="gray" fontSize={2.5} method={doubleClickHandler} />)
+      setDesc(<Info className="desc" text={e.target.value} color="gray" fontSize={1} method={doubleClickHandler} />)
     }
   }
-  const [desc, setDesc] = useState(<Info text={defaultDesc} color="gray" fontSize={2.5} method={doubleClickHandler} />); 
+  const [desc, setDesc] = useState(<Info className="desc" text={defaultDesc} color="gray" fontSize={1} method={doubleClickHandler} />); 
 
   return (
-    <div className="desc">
+    <>
       {desc}
-    </div>
+    </>
   )
 }
 
-function ProjectSource() {
+function ProjectSource({source, icon}) {
+  return (
+    <a className="projSource" href={source} target="_blank">
+      <SVGIcon icon={icon} />
+    </a>
+  )
 }
 
 function Project1() {
   const title = "A compiler for Jack Programming langauge"
-  const desc = "akjsdkjasbnfkhaskdjbasjbfkasjbndj \nkjashdkjahsdkjhasdjhkjashd \nkjashdkabskdjbasjdhbshjkdgfb asbdf hghahsjd \nashgdjhasdhjbasd \n"
+  const desc = "I'M A PARAGRAPH. CLICK HERE TO ADD YOUR OWN TEXT AND EDIT ME. IT’S EASY. JUST CLICK “EDIT TEXT” OR DOUBLE CLICK ME TO ADD YOUR OWN CONTENT AND MAKE CHANGES TO THE FONT. FEEL FREE TO DRAG AND DROP ME ANYWHERE YOU LIKE ON YOUR PAGE. "
+  const source = "https://www.github.com/"
   return (
     <div id="Project1" className="Projects">
       <ProjectTitle defaultTitle={title} />
       <ProjectDescription defaultDesc={desc}/>
-      <ProjectSource />
+      <ProjectSource source = {source} icon={gitHub}/>
     </div>    
   )
 }
 
+function Project2() {
+  const title = "Almost Twitter"
+  const desc = "I'M A PARAGRAPH. CLICK HERE TO ADD YOUR OWN TEXT AND EDIT ME. IT’S EASY. JUST CLICK “EDIT TEXT” OR DOUBLE CLICK ME TO ADD YOUR OWN CONTENT AND MAKE CHANGES TO THE FONT. FEEL FREE TO DRAG AND DROP ME ANYWHERE YOU LIKE ON YOUR PAGE. "
+  const source = "https://www.github.com/"
+  return (
+    <div id="Project1" className="Projects">
+      <ProjectTitle defaultTitle={title} />
+      <ProjectDescription defaultDesc={desc}/>
+      <ProjectSource source = {source} icon={gitHub}/>
+    </div>    
+  )
+
+}
+
+function Project3() {
+  const title = "Basic Implementation of UART"
+  const desc = "I'M A PARAGRAPH. CLICK HERE TO ADD YOUR OWN TEXT AND EDIT ME. IT’S EASY. JUST CLICK “EDIT TEXT” OR DOUBLE CLICK ME TO ADD YOUR OWN CONTENT AND MAKE CHANGES TO THE FONT. FEEL FREE TO DRAG AND DROP ME ANYWHERE YOU LIKE ON YOUR PAGE. "
+  const source = "https://www.github.com/"
+  return (
+    <div id="Project1" className="Projects">
+      <ProjectTitle defaultTitle={title} />
+      <ProjectDescription defaultDesc={desc}/>
+      <ProjectSource source = {source} icon={gitHub}/>
+    </div>    
+  )
+}
 function Projects () {
   return (
     <div className="ProjectsHolder">
       Projects
       <Project1 />
+      <Project2 />
+      <Project3 />
     </div>
   )
 }
@@ -149,6 +193,21 @@ function CV() {
   ) 
 }
 
+function Contact() {
+  return (
+    <div className="Contact">
+      <a href="https://linkedin.com/" target="_blank">
+        <SVGIcon icon={linkedin} />
+      </a>
+      <a href="https://twitter.com/" targte="_blank">
+        <SVGIcon icon={twitter} />
+      </a>
+      <a href="https://gmail.com/" targte="_blank">
+        <SVGIcon icon={gmail} />
+      </a>
+    </div>
+  )
+}
 function App() {
   return (
     <>
@@ -156,6 +215,10 @@ function App() {
         className="intro"
       >
         <CV />
+        <Contact />
+        <div class="Credits">
+          Developed by kathulu
+        </div>
       </div>
     </>
   )
